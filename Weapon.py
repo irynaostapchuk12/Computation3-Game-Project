17f0bd554a309e81a2ff1a0912424863d0c8abd8
Rodrigo Luzio das Neves <20231616@novaims.unl.pt>
2024-12-13 15:18:31 +0000

    config, weapon, interface
    added things on config, including the corbel font.
    
    weapon created

--- Weapon.py
+++ Weapon.py
@@ -1,3 +1,4 @@
+
 # direction:  left or right, if direction True = right, if direction False = left
 # image, list of different images
 #size, size of the image
@@ -13,84 +14,99 @@
 import pygame
 import math
 import config
+import avatar
 
 
 
-class Sword:
-    def __init__(self, x_character, y_character, direction_character, weapon_reach):
-        self.x_character = x_character
-        self.y_character = y_character
+class Sword(avatar):
+    def __init__(self, weapon_reach):
 
-        self.direction_character = direction_character
+        super.__init__(self.x_avatar, self.y_avatar, self.direction_avatar)
+
         self.weapon_reach = weapon_reach
 
     def attack_area(self):
         if self.direction_character: # if direction is the right
-            return pygame.Rect(self.x_character + config.width_character, self.y_character ,self.x_character + config.width_character + self.weapon_reach,  self.y_character + config.height_character)
+            return pygame.Rect(self.x_avatar + config.width_avatar, self.y_avatar, self.x_avatar + config.width_avatar + self.weapon_reach,  self.y_avatar + config.height_avatar)
 
         elif self.direction_character: # if direction is false then left
-            return pygame.Rect(self.x_character, self.y_character, - self.weapon_reach, config.height_character)
+            return pygame.Rect(self.x_avatar, self.y_avatar, - self.weapon_reach, config.height_avatar)
 
 
-class Fist:
-    def __init__(self, x_character, y_character, direction_character):
-        self.x_character = x_character
-        self.y_character = y_character
 
-        self.direction_character = direction_character
+
+class Fist(avatar):
+    def __init__(self):
+
+        super.__init__(self.x_avatar, self.y_avatar, self.direction_avatar)
+
 
 
     def attack_area(self):
         if self.direction_character: # if direction is the right
-            return pygame.Rect(self.x_character + (config.width_character//2), self.y_character, config.width_character // 2, config.height_character)
+            return pygame.Rect(self.x_avatar + (config.width_avatar//2), self.y_avatar, config.width_avatar // 2, config.height_avatar)
 
         elif self.direction_character: # if direction is false then left
-            return pygame.Rect(self.x_character + (config.width_character//2), self.y_character, - config.width_character//2, config.height_character)
+            return pygame.Rect(self.x_avatar + (config.width_avatar//2), self.y_avatar, - config.width_avatar//2, config.height_avatar)
 
 
     # tert cuidado que aqui o direction é 1 ou -1
 
-class BowArrow:
+class BowArrow(avatar):
 
-    def __init__(self, x_arrow_spawn, y_arrow_spawn, x_size, y_size, x_location, y_location, direction_character, image):
-        self.x_spawn = x_spawn
-        self.y_spawn = y_spawn
+    def __init__(self, x_location, y_location,  image):
 
-        self.x_size = x_size
-        self.y_size = y_size
+        super.__init__(self.x_avatar, self.y_avatar, self.direction_avatar)
+
+
+        self.x_arrow_spawn = self.x_avatar + (config.width_avatar//2)
+        self.y_arrow_spawn = self.y_avatar + (config.height_avatar//2)
+
 
         self.x_location = x_location
         self.y_location = y_location
 
-        self.direction_character = direction_character
         self.speed = 7
 
         self.arrow_image = pygame.image.load(f"{image}")
-        self.arrow_image = pygame.transform.scale(self.arrow_image, (self.x_size, self.y_size))
+        self.arrow_image = pygame.transform.scale(self.arrow_image, config.arrow_size)
+        self.arrow_rect = self.arrow_image.get_rect(center=(x_arrow_spawn, y_arrow_spawn))
+
+        self.left_angle = 0
+        self.right_angle = 0
 
 
-
-
-    def generate(self):
+    def arrow(self):
         # needs to have screen created
-        screen.blit(self.arrow_image, (self.x_spawn, self.y_spawn))
-
-        if self.direction_character: # for the right
-            pass
-
-        elif self.direction_character: # for the left
-            pass
+        screen.blit(self.arrow_image, (self.x_arrow_spawn, self.y_arrow_spawn))
 
 
-    def movement(self):
 
-        self.x_location += int(self.speed * math.cos(self.direction))
-        self.y_location += int(self.speed * math.sin(self.direction))
+        if self.direction_avatar: # for the right
+            rotated_arrow_image = pygame.transform.rotate(self.arrow_image, self.right_angle)
+            rotated_arrow_rect = rotated_arrow_image.get_rect(center=rotated_arrow_image.center)
+            screen.blit(rotated_arrow_image, rotated_arrow_rect)
+
+
+
+
+        else: # for the left
+            rotated_arrow_image = pygame.transform.rotate(self.arrow_image, self.left_angle)
+            rotated_arrow_rect = rotated_arrow_image.get_rect(center=rotated_arrow_image.center)
+            screen.blit(rotated_arrow_image, rotated_arrow_rect)
+
+
+
+        self.x_location += int(self.speed * math.cos(self.direction_avatar))
+        self.y_location += int(self.speed * math.sin(self.direction_avatar))
 
 
 
         # in here i need the vars of the screen size
-        if self.rect.x < 0 or self.rect.x > config.width_character or self.rect.y < 0 or self.rect.y > config.height_character:
-            self.kill()
+        if (rotated_arrow_rect < 0 or
+            rotated_arrow_rect.x > config.width_avatar or
+            rotated_arrow_rect.y < 0 or
+            rotated_arrow_rect.y > config.height_avatar):
 
+            self.kill()
 
--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 # Config file used to set global variables and other settings
 # COLORS AND PICTURES HERE FOR NOW
 from typing import Tuple
+import pygame
+import sys
+pygame.init()
+
 
 # COLORS
 dark_red = (138, 0, 0)  # Dark red for buttons
@@ -17,13 +21,26 @@ greenish = (182, 215, 168)
 
 # SCREEN RESOLUTION
 resolution = (720, 720)  # height/width
+resolution_scrollable_shop = (720, 3440)
+
 resolution_title = (350,350)
 resolution_back_jungle = (720, 720)
 resolution_credits = (600, 600)
-width, height = resolution[0], resolution[1]
+
+
+screen_width, screen_height = resolution[0], resolution[1]
+scrollable_width = resolution_scrollable_shop[0]
+scrollable_height = resolution_scrollable_shop[1]
+
 fps = 60
 
+
+
+
 # SIZES
+
+shop_image_size = (300, 300)
+coin_shop_image_size = (50, 50)
 player_size = (50, 100)
 enemy_size = (40, 40)
 bullet_size = 10
@@ -33,8 +50,79 @@ bullet_size = 10
 #CHARACTER
 height_character = 0
 width_character = 0
+
 speed_character = 0
 health_character = 0
+
+
+
+
+
+# SHOP POSITIONS
+
+shop_text_position = (360, 30)
+
+
+# 30 is the padding
+# 50 is the letter
+# 60 for space between rects
+# 30+50+60=140+110=250
+health_elixir_shop_position = (30, 140)
+speed_elixir_shop_position = (390, 140)
+
+# 250 is the previous space
+# 50 is the letter
+# 300 is the size of the image
+# 10 is the space between image and word
+# 60 is the new space between rects
+# 110 for coin and tag
+# 140+50+300+10+60=560+110=780
+invincibility_powerup_shop_position = (30,670)
+de_spawner_powerup_shop_position = (390,670)
+
+
+# 670 is previous space
+# 50 is the letter
+# 300 is the size of the image
+# 10 is the space between image and word
+# 60 is the new space between rects
+# 110 is coin and tag
+# 560+50+300+10+60=980
+double_jump_powerup_shop_position = (30,1200)
+double_coins_powerup_shop_position = (390,1200)
+
+
+chest_shop_position = (210, 2000)
+
+
+
+
+
+
+
+
+
+
+#FONT
+corbelfont = pygame.font.SysFont("Corbel", 40)
+
+
+
+
+
+
+# PRICES
+
+chest_price = 0
+health_elixir_price = 0
+speed_elixir_price = 0
+invincibility_powerup_price = 0
+de_spawner_powerup_price = 0
+double_jump_powerup_price = 0
+double_coins_powerup_price = 0
+
+
+
 
 
 # ENEMIES (change later)
