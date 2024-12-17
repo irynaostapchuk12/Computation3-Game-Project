89ccad12be817d342dfa617f5d5b4fd98daa89bb
Rita Graça <163135609+ritatgraca@users.noreply.github.com>
2024-12-17 16:00:41 +0000

    rita - platfoms
    falta o ecra eu sei

--- game.py
+++ game.py
@@ -1,11 +1,137 @@
-import math
-
-#from enemy import Enemy
-from config import *
 import pygame
-#from player import Player
+from player import Player
+from config import *
 from shed import shed
-import math
+from interface import Settings_
+
+# define the tile size
+tile_size = 130
+screen = pygame.display.set_mode((width, height))
+
+
+class World:
+    def __init__(self, data):
+        self.tile_list = []
+        self.platform_for_game = pygame.image.load("backgroundgame_level/platform_levels.png")
+        self.load_data(data)
+
+    def load_data(self, data):
+        self.tile_list = []
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(self.platform_for_game, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect, None)  # Add a timer placeholder
+                    self.tile_list.append(tile)
+
+                col_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+
+
+# Platform configurations
+world_data = [
+    # draw the first row
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0],
+    [1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+
+]
+
+world = World(world_data)
+
+
+def execute_game(player):
+    # SETUP:
+
+    # setting up the background:
+    bigimage = pygame.image.load("backgroundgame_level/background_of_level1.png").convert()
+
+    # using the clock to control the time frame.
+    clock = pygame.time.Clock()
+
+    # screen setup:
+    pygame.display.set_caption("Jungle Rex")
+
+    # MAIN GAME LOOP
+    running = True
+
+    # Background position
+    bg_x = 0
+
+    # Create a sprite group and add the player
+    all_sprites = pygame.sprite.Group()
+    all_sprites.add(player)
+
+    while running:
+
+        # controlling the frame rate
+        clock.tick(fps)
+
+        # Get the mouse position
+        mouse = pygame.mouse.get_pos()
+
+        # Update and draw all sprites
+        all_sprites.update()
+
+        # Check if the player is moving
+        # keys = pygame.key.get_pressed()
+        # if keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]:
+        # Update background position based on player movement
+        #   if keys[pygame.K_RIGHT]:
+        #      bg_x -= player.speed
+        # elif keys[pygame.K_LEFT]:
+        #    bg_x += player.speed
+
+        # Update world tiles' positions based on player movement
+        # for tile in world.tile_list:
+        #   if keys[pygame.K_RIGHT]:
+        #      tile[1].x -= player.speed
+        # elif keys[pygame.K_LEFT]:
+        #    tile[1].x += player.speed
+
+        # Draw the background image
+        screen.blit(bigimage, (bg_x, -125))
+
+        # Draw the world
+        world.draw()
+
+        # Draw all sprites
+        all_sprites.draw(screen)
+        # settings image - click
+        settings = pygame.image.load("backgroundgame_level/settings_button.png")
+
+        settings_rect = settings.get_rect(topleft=(30, 50))  # position ajusted
+
+
+        # handling events:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if settings_rect.collidepoint(event.pos):
+                    Settings_()
+
+            # showing settings image
+        screen.blit(settings, (30, 50))
+
+        # Update the display
+        pygame.display.flip()
+    pygame.display.update()
+
+    # the main while game loop has terminated and the game ends
+    pygame.quit()
 
 
 def game_loop():
@@ -15,85 +141,9 @@ def game_loop():
     # by default I start the game in the main area
     current_state = "main"
 
-    # "endeless" game loop:
+    # "endless" game loop:
     while True:
         if current_state == "main":
             current_state = execute_game(player)
         elif current_state == "shed":
             current_state = shed(player)
-
-
-def execute_game(player):
-    # SETUP:
-
-    # setting up the background:
-    bigimage = pygame.image.load("backgroundgame_level/background_of_level1.png").convert()
-    bigimagewidth = bigimage.get_width()
-    bigimage__ = pygame.transform.scale(bigimage, (bigimagewidth, 750))
-    bigimageheight = bigimage.get_height()
-
-    # background = pygame.transform.scale(image_level_one, (width, height))
-
-    # using the clock to control the time frame.
-    clock = pygame.time.Clock()
-
-    # screen setup:
-    screen = pygame.display.set_mode(screen_resolution)
-    pygame.display.set_caption("Jungle Rex")
-
-    # creating an empty group for the player (that we received as input)
-    # player_group = pygame.sprite.Group()
-
-    # adding the player to the group
-    # player_group.add(player)
-
-    # creating an empty bullet group that will be given as input to the player.shoot() method
-    # bullets = pygame.sprite.Group()
-
-    # creating the enemy group
-    # enemies = pygame.sprite.Group()
-
-    # before starting our main loop, setup the enemy cooldown variable
-    # enemy_cooldown = 0
-
-    # MAIN GAME LOOP
-    running = True
-
-    # Configurações do scroll
-    scroll_speed = 1  # Velocidade do scroll
-    offset_x = 0  # Deslocamento horizontal
-    cycles_completed = 0
-    max_cycles = 2
-    while running:
-
-        # controlling the frame rate
-        clock.tick(fps)
-
-        # handling events:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-
-        # Atualizar o deslocamento horizontal apenas se nao atingir o nr máximo de ciclos
-        if cycles_completed < max_cycles:
-            offset_x -= scroll_speed  # Mover para a esquerda
-
-            # Fim ao scroll quando a imagem sair totalmente do ecrã
-            if offset_x <= -bigimagewidth:  # -bigimagewidht -> saiu completamente da janela visível
-                offset_x = 0  # logo reinicia o deslocamento horizontal para que a imagem volte ao início
-                cycles_completed += 1 # acabou um ciclo
-
-
-
-
-        # Desenhar a imagem principal
-        screen.blit(bigimage, (offset_x, -124))  # Primeira posição da imagem
-
-        # Desenhar uma segunda cópia da imagem para o loop contínuo
-        if offset_x + bigimageheight < 720:
-            screen.blit(bigimage, (offset_x + bigimagewidth, 0))
-
-        pygame.display.flip()
-
-    # the main while game loop has terminated and the game ends
-    pygame.quit()
